7d95983870551bb2e3c92b9bc20c80193c332a05
ifirmawan <firmawaneiwan@gmail.com>
2023-10-01 20:00:30 +0700

    chore: add doc author

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Notes from "./components/Notes";
-
+/**
+ * @author: Iwan firmawan
+ */
 const root = createRoot(document.getElementById("root"));
 root.render(<Notes />);
