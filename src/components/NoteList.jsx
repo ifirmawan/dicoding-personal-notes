8710b25f5d3f393f4ac1c848f50b9950027a78fb
ifirmawan <firmawaneiwan@gmail.com>
2023-10-01 19:57:06 +0700

    feat: confirm before delete and handle switch archived status

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import { Button, ButtonGroup, ListGroup } from "react-bootstrap";
 import { showFormattedDate } from "../utils";
 
-const NoteList = ({ data = [] }) => {
+const NoteList = ({ data = [], filterNotes, setAllNotes }) => {
+  const handleOnSwitch = (id, archived = true) => {
+    const _notes = filterNotes?.map((note) =>
+      note.id === id ? { ...note, archived } : note
+    );
+    setAllNotes(_notes);
+  };
+
+  const handleOnRemove = (id, title) => {
+    if (confirm(`Hapus catatan: ${title}?`)) {
+      const _notes = filterNotes.filter((note) => note.id !== id);
+      setAllNotes(_notes);
+    }
+  };
+
   return (
     <ListGroup variant="flush">
       {data.map((d) => (
@@ -14,12 +28,36 @@ const NoteList = ({ data = [] }) => {
           </div>
           <div className="d-flex justify-content-end">
             <ButtonGroup>
-              <Button variant="outline-danger">Hapus</Button>
-              <Button variant="outline-secondary">Arsipkan</Button>
+              <Button
+                variant="outline-danger"
+                onClick={() => handleOnRemove(d.id, d.title)}
+              >
+                Hapus
+              </Button>
+              {d.archived ? (
+                <Button
+                  variant="primary"
+                  onClick={() => handleOnSwitch(d.id, false)}
+                >
+                  Pindahkan
+                </Button>
+              ) : (
+                <Button
+                  variant="secondary"
+                  onClick={() => handleOnSwitch(d.id)}
+                >
+                  Arsipkan
+                </Button>
+              )}
             </ButtonGroup>
           </div>
         </ListGroup.Item>
       ))}
+      {data.length === 0 && (
+        <div className="w-100 p-6 text-center">
+          <p className="fst-italic">Tidak ada catatan</p>
+        </div>
+      )}
     </ListGroup>
   );
 };
